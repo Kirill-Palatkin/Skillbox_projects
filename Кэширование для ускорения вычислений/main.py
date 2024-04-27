eb414122ece329f58dc3175e24b6ac16802456f5
Kirill-Palatkin <110474394+Kirill-Palatkin@users.noreply.github.com>
2024-04-27 21:48:50 +0300

    Update main.py

--- Кэширование для ускорения вычислений/main.py
+++ Кэширование для ускорения вычислений/main.py
@@ -1,1 +1,31 @@
+import functools
 
+
+def caching(func):
+    cache = dict()
+
+    @functools.wraps(func)
+    def wrapped_func(*args):
+        if args not in cache:
+            cache[args] = func(*args)
+        return cache[args]
+    return wrapped_func
+
+
+@caching
+def fibonacci(number):
+    if number <= 1:
+        return number
+    return fibonacci(number - 1) + fibonacci(number - 2)
+
+
+# Вычисление числа Фибоначчи с использованием кеширования
+print(fibonacci(10))  # Результат будет кеширован
+
+# Повторное вычисление числа Фибоначчи с теми же аргументами
+print(fibonacci(10))  # Результат будет взят из кеша
+
+# Вычисление числа Фибоначчи с другим аргументом
+print(fibonacci(5))  # Результат будет вычислен и закеширован
+
+print(fibonacci(500))
